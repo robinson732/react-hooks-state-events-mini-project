1caa144ce2aaa1044893d6d2cd711d072495d973
robinson732 <kimanirobinson336@gmail.com>
2025-07-27 23:02:25 +0300

    final

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onCategoryChange }) {
+  const buttons = categories.map((cat) => (
+    <button
+      key={cat}
+      className={selectedCategory === cat ? "selected" : ""}
+      onClick={() => onCategoryChange(cat)}
+    >
+      {cat}
+    </button>
+  ));
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task() {
+function Task({ text, category, onDeleteTask }) {
+  function handleDelete() {
+    onDeleteTask(text); // Pass the task identifier (text) back to App
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
